a68ed03c2aae56d2d3d4ccfacefe7adb6d7eeeaf
swolmer <sophiawolmer@gmail.com>
2025-07-08 10:03:12 -0400

    Add files via upload

---
+++ init_upload.py
@@ -0,0 +1,5 @@
+# Section: TEMP UPLOAD EXECUTOR — NOT part of main script
+from main_script_orgid import handle_uploaded_zip
+
+# Replace with your actual zip name and org ID
+handle_uploaded_zip("plastic_surgery_emory.zip", org_id="emory")
