7327b7dfd4edf180043d88b78ab5ac537c614b4b
swolmer <sophiawolmer@gmail.com>
2025-07-11 10:11:24 -0400

    Delete test_query.py

--- test_query.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.post("http://127.0.0.1:8000/query", json={
-    "question": "What are the anatomical considerations for nasolabial filler injection?"
-})
-print(response.json())
